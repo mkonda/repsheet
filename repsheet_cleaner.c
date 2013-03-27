15afdbbca252fb55ffbd30f32203d7e3fb945254
Aaron Bedra <aaron@aaronbedra.com>
2013-03-27 15:21:33 -0500

    Adding repsheet cleanup so that stale keys can be removed in the background

---
+++ repsheet_cleaner.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include "hiredis/hiredis.h"
+
+int main(int argc, char *argv[])
+{
+  int i, c;
+  redisContext *context;
+  redisReply *repsheet_members, *reply;
+  char *host = "localhost";
+  int port = 6379;
+
+  while((c = getopt (argc, argv, "h:p:")) != -1)
+    switch(c)
+      {
+      case 'h':
+        host = optarg;
+        break;
+      case 'p':
+        port = atoi(optarg);
+        break;
+      case '?':
+        fprintf(stderr, "Unknown option: '\\x%x'.\n", optopt);
+        return 1;
+      default:
+        abort();
+      }
+
+  printf("Starting Repsheet cleanup on %s:%d\n", host, port);
+
+  context = redisConnect(host, port);
+  if (context == NULL || context->err) {
+    if (context) {
+      printf("Redis Connection Error: %s\n", context->errstr);
+      redisFree(context);
+    } else {
+      perror("Connection Error: can't allocate redis context\n");
+      redisFree(context);
+    }
+    return -1;
+  }
+
+  repsheet_members = redisCommand(context, "SMEMBERS repsheet");
+
+  for (i = 0; i < repsheet_members->elements; i++) {
+    reply = redisCommand(context, "EXISTS %s", repsheet_members->element[i]->str);
+    if (reply->integer == 0) {
+      printf("  Removing %s\n", repsheet_members->element[i]->str);
+      freeReplyObject(redisCommand(context, "SREM repsheet %s", repsheet_members->element[i]->str));
+    }
+    freeReplyObject(reply);
+  }
+
+  freeReplyObject(repsheet_members);
+  redisFree(context);
+
+  printf("Repsheet cleanup complete\n");
+
+  return 0;
+}
