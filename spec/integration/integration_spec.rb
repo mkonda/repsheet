d38b07be29e5d662a3644dcb61afcba252fd831d
Aaron Bedra <aaron@aaronbedra.com>
2013-03-31 13:39:43 -0500

    Fixing up tests and renaming local conf to modsecurity.
    It turns out using a web driver when I could have done this headless was
    pretty dumb, so I changed to using Curb. I should have done that in the
    first place.

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -2,15 +2,11 @@ require 'spec_helper'
 
 describe "Repsheet Bootstrap" do
   it "Redis is running" do
-    redis = Redis.new
-    redis.ping.should == "PONG"
+    Redis.new.ping.should == "PONG"
   end
 
   it "Apache is running" do
-    driver = Selenium::WebDriver.for :firefox
-    driver.get "http://localhost:8888"
-    driver.find_element(:xpath, '//h1').text.should == "It works!"
-    driver.close
+    Curl.get("http://localhost:8888").body_str.should == "<html><body><h1>It works!</h1></body></html>"
   end
 end
 
@@ -18,48 +14,47 @@ describe "Repsheet Recorder" do
   it "Records the IP, User Agent, Method, URI, and Arguments during a request" do
     redis = Redis.new
     redis.flushdb
-    driver = Selenium::WebDriver.for :firefox
-    driver.get "http://localhost:8888"
-    redis = Redis.new
+
+    Curl.get "http://localhost:8888"
+    sleep(1)
+
     redis.llen("127.0.0.1").should == 1
-    driver.close
   end
 
   it "Trims the number of entries for an IP to the number specified" do
-    system("sed -i.bak 's/RepsheetRedisMaxLength 1000/RepsheetRedisMaxLength 2/' build/conf/httpd.conf")
-    system("build/bin/apachectl restart")
-    sleep(5)
     redis = Redis.new
     redis.flushdb
-    driver = Selenium::WebDriver.for :firefox
+
+    c = Curl::Easy.new
     3.times do
-      driver.get "http://localhost:8888"
+      c.url = "http://localhost:8888"
+      c.perform 
     end
-    redis = Redis.new
+
     redis.llen("127.0.0.1").should == 2
-    system("sed -i.bak 's/RepsheetRedisMaxLength 2/RepsheetRedisMaxLength 1000/' build/conf/httpd.conf")
-    driver.close
   end
 end
 
 describe "Repsheet ModSecurity Integration" do
   it "Creates the proper Redis keys when a security rule is triggered" do
-    driver = Selenium::WebDriver.for :firefox
-    driver.get "http://localhost:8888?../../"
     redis = Redis.new
+    redis.flushdb
+
+    Curl.get "http://localhost:8888?../../"
+
     redis.type("950103:127.0.0.1").should == "string"
     redis.type("950103").should == "set"
     redis.type("repsheet").should == "set"
-    driver.close
   end
 
   it "Adds the offending IP address to the repsheet" do
     redis = Redis.new
     redis.flushdb
+
     redis.sismember("repsheet", "127.0.0.1").should be_false
-    driver = Selenium::WebDriver.for :firefox
-    driver.get "http://localhost:8888?../../"
+
+    Curl.get "http://localhost:8888?../../"
+
     redis.sismember("repsheet", "127.0.0.1").should be_true
-    driver.close
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
 require 'rspec'
 require 'redis'
-require 'selenium-webdriver'
+require 'curb'
+
