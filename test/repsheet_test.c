a2fc5aa7e0a8f8bb24cd485e0c3fc9c8ebe6aeba
Aaron Bedra <aaron@aaronbedra.com>
2013-06-03 15:28:39 -0500

    Adding tests for repsheet_ip_lookup

--- test/repsheet_test.c
+++ test/repsheet_test.c
@@ -2,7 +2,7 @@
 #include "test_suite.h"
 
 redisContext *context;
-redisReply * reply;
+redisReply *reply;
 
 void setup(void)
 {
@@ -15,7 +15,10 @@ void setup(void)
 
 void teardown(void)
 {
-  freeReplyObject(reply);
+  freeReplyObject(redisCommand(context, "flushdb"));
+  if (!reply == NULL) {
+    freeReplyObject(reply);
+  }
   redisFree(context);
 }
 
@@ -73,18 +76,57 @@ START_TEST(properly_records_arguments)
 }
 END_TEST
 
-Suite *make_repsheet_suite(void) {
-  Suite *suite = suite_create("Repsheet");
+START_TEST(repsheet_ip_lookup_returns_allow_when_not_on_repsheet_or_blacklist)
+{
+  ck_assert_int_eq(repsheet_ip_lookup(context, "1.1.1.1"), ALLOW);
+}
+END_TEST
 
-  TCase *tc_repsheet = tcase_create("repsheet_record");
-  tcase_add_checked_fixture(tc_repsheet, setup, teardown);
-  tcase_add_test(tc_repsheet, handles_null_values);
-  tcase_add_test(tc_repsheet, properly_records_timestamp);
-  tcase_add_test(tc_repsheet, properly_records_user_agent);
-  tcase_add_test(tc_repsheet, properly_records_http_method);
-  tcase_add_test(tc_repsheet, properly_records_arguments);
-  tcase_add_test(tc_repsheet, properly_records_uri);
-  suite_add_tcase(suite, tc_repsheet);
+START_TEST(repsheet_ip_lookup_returns_allow_when_on_the_whitelist)
+{
+  freeReplyObject(redisCommand(context, "SET 1.1.1.1:repsheet:blacklist true"));
+  freeReplyObject(redisCommand(context, "SET 1.1.1.1:repsheet:whitelist true"));
+
+  ck_assert_int_eq(repsheet_ip_lookup(context, "1.1.1.1"), ALLOW);
+}
+END_TEST
+
+START_TEST(repsheet_ip_lookup_returns_notify_when_on_the_repsheet)
+{
+  freeReplyObject(redisCommand(context, "SET 1.1.1.1:repsheet true"));
+
+  ck_assert_int_eq(repsheet_ip_lookup(context, "1.1.1.1"), NOTIFY);
+}
+END_TEST
+
+START_TEST(repsheet_ip_lookup_returns_block_when_on_the_blacklist)
+{
+  freeReplyObject(redisCommand(context, "SET 1.1.1.1:repsheet:blacklist true"));
+
+  ck_assert_int_eq(repsheet_ip_lookup(context, "1.1.1.1"), BLOCK);
+}
+END_TEST
+
+Suite *make_repsheet_suite(void) {
+  Suite *suite = suite_create("librepsheet");
+
+  TCase *tc_record = tcase_create("repsheet_record");
+  tcase_add_checked_fixture(tc_record, setup, teardown);
+  tcase_add_test(tc_record, handles_null_values);
+  tcase_add_test(tc_record, properly_records_timestamp);
+  tcase_add_test(tc_record, properly_records_user_agent);
+  tcase_add_test(tc_record, properly_records_http_method);
+  tcase_add_test(tc_record, properly_records_arguments);
+  tcase_add_test(tc_record, properly_records_uri);
+  suite_add_tcase(suite, tc_record);
+
+  TCase *tc_ip_lookup = tcase_create("repsheet_ip_lookup");
+  tcase_add_checked_fixture(tc_ip_lookup, setup, teardown);
+  tcase_add_test(tc_ip_lookup, repsheet_ip_lookup_returns_allow_when_not_on_repsheet_or_blacklist);
+  tcase_add_test(tc_ip_lookup, repsheet_ip_lookup_returns_allow_when_on_the_whitelist);
+  tcase_add_test(tc_ip_lookup, repsheet_ip_lookup_returns_notify_when_on_the_repsheet);
+  tcase_add_test(tc_ip_lookup, repsheet_ip_lookup_returns_block_when_on_the_blacklist);
+  suite_add_tcase(suite, tc_ip_lookup);
 
   return suite;
 }
