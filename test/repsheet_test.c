fc116a1f4b7f79542e287ecd8fdfb4626efab655
Aaron Bedra <aaron@aaronbedra.com>
2013-06-13 11:08:52 -0500

    Re adding key expiry

--- test/repsheet_test.c
+++ test/repsheet_test.c
@@ -1,6 +1,9 @@
 #include "../src/repsheet.h"
 #include "test_suite.h"
 
+#define TTL 200
+#define REDIS_MAX_LENGTH 10
+
 redisContext *context;
 redisReply *reply;
 
@@ -24,7 +27,7 @@ void teardown(void)
 
 START_TEST(handles_null_values)
 {
-  repsheet_record(context, NULL, NULL, NULL, NULL, NULL, "1.1.1.1", 100);
+  repsheet_record(context, NULL, NULL, NULL, NULL, NULL, "1.1.1.1", REDIS_MAX_LENGTH, TTL);
 
   reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 0");
   ck_assert_str_eq(reply->element[0]->str, "-, -, -, -, -");
@@ -33,7 +36,7 @@ END_TEST
 
 START_TEST(properly_records_timestamp)
 {
-  repsheet_record(context, "12345", NULL, NULL, NULL, NULL, "1.1.1.1", 100);
+  repsheet_record(context, "12345", NULL, NULL, NULL, NULL, "1.1.1.1", REDIS_MAX_LENGTH, TTL);
 
   reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 0");
   ck_assert_str_eq(reply->element[0]->str, "12345, -, -, -, -");
@@ -42,7 +45,7 @@ END_TEST
 
 START_TEST(properly_records_user_agent)
 {
-  repsheet_record(context, NULL, "A User Agent", NULL, NULL, NULL, "1.1.1.1", 100);
+  repsheet_record(context, NULL, "A User Agent", NULL, NULL, NULL, "1.1.1.1", REDIS_MAX_LENGTH, TTL);
 
   reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 0");
   ck_assert_str_eq(reply->element[0]->str, "-, A User Agent, -, -, -");
@@ -51,7 +54,7 @@ END_TEST
 
 START_TEST(properly_records_http_method)
 {
-  repsheet_record(context, NULL, NULL, "GET", NULL, NULL, "1.1.1.1", 100);
+  repsheet_record(context, NULL, NULL, "GET", NULL, NULL, "1.1.1.1", REDIS_MAX_LENGTH, TTL);
 
   reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 0");
   ck_assert_str_eq(reply->element[0]->str, "-, -, GET, -, -");
@@ -60,7 +63,7 @@ END_TEST
 
 START_TEST(properly_records_uri)
 {
-  repsheet_record(context, NULL, NULL, NULL, "/", NULL, "1.1.1.1", 100);
+  repsheet_record(context, NULL, NULL, NULL, "/", NULL, "1.1.1.1", REDIS_MAX_LENGTH, TTL);
 
   reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 0");
   ck_assert_str_eq(reply->element[0]->str, "-, -, -, /, -");
@@ -69,10 +72,42 @@ END_TEST
 
 START_TEST(properly_records_arguments)
 {
-  repsheet_record(context, NULL, NULL, NULL, NULL, "foo=bar", "1.1.1.1", 100);
+  repsheet_record(context, NULL, NULL, NULL, NULL, "foo=bar", "1.1.1.1", REDIS_MAX_LENGTH, TTL);
 
   reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 0");
   ck_assert_str_eq(reply->element[0]->str, "-, -, -, -, foo=bar");
+}
+END_TEST
+
+START_TEST(properly_sets_expiry_on_records)
+{
+  repsheet_record(context, NULL, NULL, NULL, NULL, NULL, "1.1.1.1", REDIS_MAX_LENGTH, TTL);
+
+  reply = redisCommand(context, "TTL 1.1.1.1:requests");
+  ck_assert_int_gt(reply->integer, 0);
+}
+END_TEST
+
+START_TEST(does_not_set_expiry_on_records_when_expiry_is_zero)
+{
+  repsheet_record(context, NULL, NULL, NULL, NULL, NULL, "1.1.1.1", REDIS_MAX_LENGTH, 0);
+
+  reply = redisCommand(context, "TTL 1.1.1.1:requests");
+  ck_assert_int_eq(reply->integer, -1);
+
+  reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 0");
+  ck_assert_str_eq(reply->element[0]->str, "-, -, -, -, -");
+}
+END_TEST
+
+START_TEST(properly_trims_length_of_stored_records)
+{
+  repsheet_record(context, NULL, NULL, NULL, NULL, NULL, "1.1.1.1", 2, TTL);
+  repsheet_record(context, NULL, NULL, NULL, NULL, NULL, "1.1.1.1", 2, TTL);
+  repsheet_record(context, NULL, NULL, NULL, NULL, NULL, "1.1.1.1", 2, TTL);
+
+  reply = redisCommand(context, "LRANGE 1.1.1.1:requests 0 -1");
+  ck_assert_int_eq(reply->elements, 2);
 }
 END_TEST
 
@@ -138,6 +173,9 @@ Suite *make_repsheet_suite(void) {
   tcase_add_test(tc_record, properly_records_http_method);
   tcase_add_test(tc_record, properly_records_arguments);
   tcase_add_test(tc_record, properly_records_uri);
+  tcase_add_test(tc_record, properly_sets_expiry_on_records);
+  tcase_add_test(tc_record, does_not_set_expiry_on_records_when_expiry_is_zero);
+  tcase_add_test(tc_record, properly_trims_length_of_stored_records);
   suite_add_tcase(suite, tc_record);
 
   TCase *tc_ip_lookup = tcase_create("repsheet_ip_lookup");
