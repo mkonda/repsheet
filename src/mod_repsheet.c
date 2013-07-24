83552cecab9402d80cbe3bca24b03140c620c40e
Aaron Bedra <aaron@aaronbedra.com>
2013-07-24 11:43:55 -0500

    Don't hang on to the Redis connection when blocking

--- src/mod_repsheet.c
+++ src/mod_repsheet.c
@@ -256,10 +256,12 @@ static int repsheet_lookup(request_rec *r)
   if (action) {
     if (action == BLOCK) {
       ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "%s %s was blocked by the repsheet", config.prefix, ip);
+      redisFree(context);
       return HTTP_FORBIDDEN;
     } else if (action == NOTIFY) {
       if (config.action == BLOCK) {
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "%s %s was blocked by the repsheet", config.prefix, ip);
+	redisFree(context);
         return HTTP_FORBIDDEN;
       } else {
         ap_log_error(APLOG_MARK, APLOG_ERR, 0, r->server, "%s IP Address %s was found on the repsheet. No action taken", config.prefix, ip);
